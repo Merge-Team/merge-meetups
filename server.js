25e38f1b2abb97a54cc7c7ab3028c7d333307cca
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-30 12:17:16 -0800

    run first format

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-const PORT = process.env.PORT|| 1111;
+const PORT = process.env.PORT || 1111;
 
 //connect to mongo call
 connectDB();
 
 //basic api
-app.get('/',(req,res) => res.send("API running"));
+app.get('/', (req, res) => res.send('API running'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
